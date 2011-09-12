3bf5981c941bafb8e675e0c4aa3820ea509fb900
Greg Wilburn <snowinferno@gmail.com>
2011-09-11 21:25:47 -0700

    commit for debug purposes

--- Plugin/lib/main.js
+++ Plugin/lib/main.js
@@ -6,8 +6,57 @@ var widget = widgets.Widget({
   label: "Mozilla website",
   contentURL: "http://www.mozilla.org/favicon.ico",
   onClick: function() {
-    tabs.open("http://www.mozilla.org/");
+//    tabs.open("http://www.google.com/");
+    getList();
   }
 });
+
+function getList(){
+    var request = require( "request" ).Request;
+    var authCode = "";
+    var auth = request({
+        url: 'https://www.google.com/accounts/ClientLogin',
+        method: 'POST',
+        content: 'Email=gwilb.ad@gmail.com&Passwd=XXXXXXX&accountType=HOSTED_OR_GOOGLE&service=writely&source=googleDocs@gwilburn.com',
+	headers: {
+	    'GData-Version': '3.0'
+	},
+/*	conent: { Email: 'gwilb.ad@gmail.com',
+		  Passwd: 'XXXXXXXXXX',
+		  accountType: 'HOSTED_OR_GOOGLE',
+		  service: 'writely',
+		  source: 'googleDocs@gwilburn.com' },*/
+        onComplete: function( resp ){
+            var re = /Auth=([a-z0-9_\-]+)/i;
+//            console.log( resp.text );
+            authCode = re.exec( resp.text )[0].split('=')[1];
+
+	    var hdr = { 'GData-Version': '3.0' };
+	    hdr['Authorization'] = authCode;
+	    var list = request( {
+//		url: 'https://docs.google.com/feeds/default/private/full',
+		url: 'http://www.gwilburn.com/headers.php',
+		headers: {
+		    'Authorization': 'GoogleLogin auth=' + authCode,
+		    'GData-Version': '3.0'
+		},
+//		method: 'POST',
+//		content: 'Email=gwilb.ad@gmail.com&Passwd=4cx10m00&accountType=HOSTED_OR_GOOGLE&service=writely&source=googleDocs@gwilburn.com',
+		content: 'Email=gwilb.ad@gmail.com&Passwd=XXXXXXXXX&accountType=HOSTED_OR_GOOGLE&service=writely&source=googleDocs@gwilburn.com',
+/*		content: { Email: 'gwilb.ad@gmail.com',
+			   Passwd: 'XXXXXXXXX',
+			   accountType: 'HOSTED_OR_GOOGLE',
+			   service: 'writely',
+			   source: 'googleDocs@gwilburn.com' },*/
+		contentType: 'text/plain',
+		onComplete: function( resp ) {
+		    console.log( resp.text );
+		}
+	    } );
+	    list.post();
+        }
+    });
+    auth.post();
+}
 
 console.log("The add-on is running.");
